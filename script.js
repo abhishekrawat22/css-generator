23111e795fd93308cbbe60aef4824c5ec5ca5e14
abhishekrawat22 <abhishekrawat5743@gmail.com>
2023-08-21 22:05:51 +0530

    Added validation and copied content notification.

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 let allLabels = document.querySelectorAll(".generator-content label");
+let globalWrappers = document.querySelectorAll('.global-wrapper .input-wrapper');
 let selector = document.querySelector("#css-selector");
 let baseFont = document.querySelector("#base-size");
 let generatedCss = document.querySelector("#generated-css");
 let generatedBtn = document.querySelector("#generate");
 let copyIcon = document.querySelector("#copy-css");
+let textareaWrapper = document.querySelector(".generated-css-wrapper");
 
 // This will generate CSS taking all the filled values.
 generatedBtn.addEventListener('click', () => {
     let generatedObj = {};
     let objValues = [];
-    allLabels.forEach((label) => {
-        let inputEle = document.querySelector("#"+label.htmlFor);
-        if (inputEle.value != "") {
-            if(inputEle.getAttribute('type') === 'number') {
-                generatedObj[label.htmlFor] = (inputEle.value/baseFont.value) + "rem";
+    if(selector.value.length > 0 && baseFont.value.length > 0) {
+        globalWrappers.forEach((globalWrapper) => {
+            globalWrapper.classList.remove('error');
+        });
+        allLabels.forEach((label) => {
+            let inputEle = document.querySelector("#"+label.htmlFor);
+            if (inputEle.value != "") {
+                if(inputEle.getAttribute('type') === 'number') {
+                    generatedObj[label.htmlFor] = (inputEle.value/baseFont.value) + "rem";
+                }
+                else {
+                    generatedObj[label.htmlFor] = inputEle.value;
+                }
             }
-            else {
-                generatedObj[label.htmlFor] = inputEle.value;
-            }
+        })
+        for (const [key, value] of Object.entries(generatedObj)) {
+            objValues.push("\t" + `${key}: ${value}` + ";\n");
         }
-    })
-    for (const [key, value] of Object.entries(generatedObj)) {
-        objValues.push("\t" + `${key}: ${value}` + ";\n");
+        generatedCss.innerHTML = selector.value + " {" + "\n" + objValues.join("") + "}";
     }
-    generatedCss.innerHTML = selector.value + " {" + "\n" + objValues.join("") + "}";
+    else {
+        globalWrappers.forEach((globalWrapper) => {
+            globalWrapper.classList.add('error');
+        })
+    }
 })
 
 // To copy CSS from the textarea on the click of a copy icon.
 copyIcon.addEventListener("click", () => {
-    // Select the textarea content when copied.
-    generatedCss.select();
-    // generatedCss.setSelectionRange(0, 99999); // For mobile devices
     // Copy the CSS inside the textarea.
     navigator.clipboard.writeText(generatedCss.value);
+    textareaWrapper.classList.add("copied")
+    setTimeout(() => {
+        textareaWrapper.classList.remove("copied")
+    }, 3000);
 })
